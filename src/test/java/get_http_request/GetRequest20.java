a5adb5de96ae131a8f0f3fe72529389077693018
Mehmet Emin Araz <mehmeteminaraz@gmail.com>
2022-03-07 23:04:23 +0300

    Api/03/07

---
+++ src/test/java/get_http_request/GetRequest20.java
@@ -0,0 +1,17 @@
+package get_http_request;
+
+public class GetRequest20 {
+    /*
+https://jsonplaceholder.typicode.com/todos/2
+1) Status kodunun 200,
+2) respose body'de,
+         "completed": değerinin false
+         "title": değerinin "quis ut nam facilis et officia qui"
+         "userId" sinin 1 ve
+    header değerlerinden
+         "via" değerinin "1.1 vegur" ve
+         "Server" değerinin "cloudflare" olduğunu test edin…
+*/
+
+
+}
